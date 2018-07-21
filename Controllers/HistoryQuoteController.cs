84aa4cd1d685e6661aac17d39093fa9d415b00d6
rx <rexin.liu@gmail.com>
2018-07-21 15:31:08 +0800

    raw data

---
+++ Controllers/HistoryQuoteController.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+  
+    public class HistoryQuoteController : ApiController
+    {
+        Ticks20sec[] products = new Ticks20sec[]
+       {
+            new Ticks20sec { exchange_id = "Tomato Soup" },
+            new Ticks20sec { exchange_id = "Tomato Soup" },
+            new Ticks20sec  { exchange_id = "Tomato Soup" }
+       };
+        //public IEnumerable<Ticks20sec> GetAllProducts()
+        //{
+        //    return products;
+        //}
+        //    
+
+        //   [HttpGet, ActionName("GetByDate")]
+        [HttpGet, Route("api/HistoryQuote/GetByDate/{dt}")]       
+        public string GetByDate(int dt )
+        {
+            DataTable dtable = VC.Tools.Helper.MysqlClientDBHelper.GetDataTable("select * from dbcoin.rw_tick_20s where dt="+dt +" order by timestamp ");
+
+            return JsonConvert.SerializeObject(dtable);
+        }
+
+
+        [HttpGet, Route("api/HistoryQuote/GetSymbol/{dt}")]
+        public string GetSymbol(int dt)
+        {
+            DataTable dtable = VC.Tools.Helper.MysqlClientDBHelper.GetDataTable("select distinct symbol from dbcoin.rw_tick_20s where dt=" + dt);
+
+            return JsonConvert.SerializeObject(dtable);
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using System.Web.Routing;
 
 namespace Web
 {
@@ -13,9 +15,15 @@ namespace Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+
+            formatters.Remove(formatters.XmlFormatter);
+
         }
     }
 }
